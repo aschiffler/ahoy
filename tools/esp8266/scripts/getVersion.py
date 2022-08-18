8dfa2ba5474b91259559f73738c9441e3d85b257
Andreas Schiffler <schiffler@roschi.eu>
2022-08-18 18:11:08 +0200

    Update getVersion.py

--- tools/esp8266/scripts/getVersion.py
+++ tools/esp8266/scripts/getVersion.py
@@ -9,12 +9,14 @@ def readVersion(path, infile):
     today = date.today()
     search = ["_MAJOR", "_MINOR", "_PATCH"]
     version = today.strftime("%y%m%d") + "_ahoy_"
+    versionnumer = "ahoy_v"
     for line in lines:
         if(line.find("VERSION_") != -1):
             for s in search:
                 p = line.find(s)
                 if(p != -1):
                     version += line[p+13:].rstrip() + "."
+                    versionnumber += line[p+13:].rstrip() + "."
     
     os.mkdir(path + ".pio/build/out/")
     sha = os.getenv("SHA",default="sha")
@@ -22,6 +24,8 @@ def readVersion(path, infile):
     src = path + ".pio/build/esp8266-release/firmware.bin"
     dst = path + ".pio/build/out/" + versionout
     os.rename(src, dst)
+    print("::set-output name=name::" + versionnumber )
     
     
 readVersion("../", "defines.h")
+
